b89a711c4a62911d92a202f7aa43fc7034ced9e6
yash srivastava <yashsrivastava087@gmail.com>
2025-04-21 14:49:42 +0530

    update

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover.jsx'
+import { Avatar, AvatarImage } from '../ui/avatar.jsx'
 
 const Navbar = () => {
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'> Internship <span className='text-[#F83002]'>Portal </span></h1>
+                    <h1 className='text-2xl font-bold'> Intern<span className='text-[#a3bc00]'>connectX </span></h1>
                 </div>
 
-                <div>
+                <div className=' flex items-center gap-6'>
                     <ul className='flex font-medium items-center gap-5'>
                         <li>Home</li>
                         <li>Jobs</li>
                         <li>Browse</li>
 
                     </ul>
+
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            </Avatar>
+                        </PopoverTrigger>
+
+                        <PopoverContent>
+                        <h1>hello</h1>
+                    </PopoverContent>
+                    </Popover>
                 </div>
             </div>
-
-
         </div>
     )
 }
